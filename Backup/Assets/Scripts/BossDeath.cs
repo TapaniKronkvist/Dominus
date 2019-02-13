b5f81b2268b125b3abc7514b2458e91fe6ed74dd
Juice-f <47383418+Juice-f@users.noreply.github.com>
2019-02-13 23:08:04 +0100

    Gjorde turretenemy finare. Skapade dödseffekter åt minibossarna, Flyttade TestBomb.Prefab till tempfiles. Flyttade lookatcamera till scripts mappen

---
+++ Backup/Assets/Scripts/BossDeath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDeath : MonoBehaviour
+{
+    [SerializeField]
+    WorldManager.Bosses beatenBoss;
+    [SerializeField]
+    float timeTillDone, beamUpSpeed;
+    [SerializeField]
+    LineRenderer line;
+
+    private void Start()
+    {
+        StartCoroutine(BossDead(timeTillDone));
+    }
+
+    private void Update()
+    {
+        line.SetPosition(1, line.GetPosition(1) + new Vector3(0, beamUpSpeed * Time.deltaTime, 0));
+    }
+
+    IEnumerator BossDead(float time)
+    {
+        yield return new WaitForSeconds(time);
+        WorldManager.ins.DefeatBoss(beatenBoss);
+    }
+
+
+}
