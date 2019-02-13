a9cf81599518220e51c1692f9e9fb707b8b8ce64
TapaniKronkvist <45991745+TapaniKronkvist@users.noreply.github.com>
2019-02-13 15:24:47 +0100

    Collection scene done

---
+++ Backup/Assets/Scripts/UI/UiCollection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiCollection : MonoBehaviour
+{
+    public Text chapter1;
+    AudioSource source;
+    public AudioClip clip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        clip = GetComponent<AudioSource>().clip;
+
+        chapter1.GetComponent<Text>().enabled = false;
+    }
+
+    public void OpenChapterOne()
+    {
+        if (chapter1.GetComponent<Text>().enabled == false)
+        {
+            chapter1.GetComponent<Text>().enabled = true;
+            source.PlayOneShot(clip);
+        }
+        else
+        {
+            chapter1.GetComponent<Text>().enabled = false;
+            source.PlayOneShot(clip);
+        }
+        
+    }
+}
