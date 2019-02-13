1723e7a3cd5d224f1cc79e3c110a5c1253b420f4
TapaniKronkvist <45991745+TapaniKronkvist@users.noreply.github.com>
2019-02-13 13:42:17 +0100

    UI hearts
    Ligger som en prefab. Dra in den i scenen eller lägg in den som en child i player. Våga inte göra det själv så jag inte skriver över nått.

--- Backup/Assets/Scripts/PlayerUi.cs
+++ Backup/Assets/Scripts/PlayerUi.cs
@@ -9,48 +9,55 @@ public class PlayerUi : MonoBehaviour
     Image createHeart;
     public Image heart;
     Playermanager getPlayerInfo;
+    public List<Image> currentHearts;
     [SerializeField] int playerHp;
-    //public int testhealth = 8;
-    GameObject player;
-    Vector3 xOffset;
-    float x = 50;
-    float yOffset;
-    
+    private int hpChange = 4;
+    private int hp;
+    private GameObject player;
+    private Vector3 xOffset;
+    private float x = 50;
+    private float yOffset;
     
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        getPlayerInfo = Playermanager.ins;
-        
+        getPlayerInfo = Playermanager.ins; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (player != null)
         {
-            playerHp = getPlayerInfo.CurrentHP;
-            print(playerHp);
-            createHeart = Instantiate(heart) as Image;
-            createHeart.transform.SetParent(transform, false);//keeps local pos
-        }
-        
+            hp = getPlayerInfo.CurrentHP;
+            hpChange = hp;
+            if (hpChange != playerHp)
+            {
+                UpdateHealth(hp);
+            }
 
-        
+        }
     }
     public void UpdateHealth(int hp)
     {
+        foreach (var item in currentHearts)
+        {
+            Destroy(item);
+        }
+        currentHearts = new List<Image>();
+        x = 25;
         for (int i = 0; i < hp; i++)
         {
-            
-                createHeart = Instantiate(heart) as Image;
-                createHeart.transform.SetParent(transform, false);//keeps local pos
-                xOffset = createHeart.transform.position;
-                xOffset.x += x;
-                createHeart.transform.position = xOffset;
-                x += 100;
+            createHeart = Instantiate(heart) as Image;
+            currentHearts.Add(createHeart);
+            createHeart.transform.SetParent(transform, false);//keeps local pos
+            xOffset = createHeart.transform.position;
+            xOffset.x += x;
+            createHeart.transform.position = xOffset;
+            x += 50;
         }
-        
+        playerHp = hpChange;
     }
 }
